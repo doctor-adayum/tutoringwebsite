5e32c3480ced431c004ff47d2e64b9ca12adf0bf
doctor-adayum <ajacob7111997@gmail.com>
2016-11-25 23:38:20 +1000

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
   <meta name="theme-color" content="#1976D2">
-  <title>On The Mark Tutoring</title>
+  <title><?php echo get_bloginfo( 'name' ); ?></title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -19,12 +19,11 @@
   <nav class="cyan" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Home</a></li>
         <?php wp_list_pages( '&title_li=' ); ?>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
+        <?php wp_list_pages( '&title_li=' ); ?>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
