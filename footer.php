c7a58cfef521c7fca62a9bc66660cf9680b7b460
doctor-adayum <ajacob7111997@gmail.com>
2016-11-25 23:02:56 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+          <p class="grey-text text-lighten-4"><?php the_author_meta( 'description' ); ?></p>
 
 
         </div>
@@ -29,7 +29,7 @@
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      Made by Adam Jacob
       </div>
     </div>
   </footer>
