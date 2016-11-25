af9cdc96e8deb2dbf730475599e8d0fecc62273e
doctor-adayum <ajacob7111997@gmail.com>
2016-11-25 22:20:09 +1000

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php bloginfo('template_directory');?>/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php bloginfo('template_directory');?>/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
   
   
@@ -123,8 +123,8 @@
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
+  <script src="<?php bloginfo('template_directory');?>/js/materialize.js"></script>
+  <script src="<?php bloginfo('template_directory');?>/js/init.js"></script>
 
   </body>
 </html>
