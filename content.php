b468e1ac30f12c7c7d2c304de1c29ad46853126e
doctor-adayum <ajacob7111997@gmail.com>
2016-11-25 23:21:05 +1000

    Update content.php

--- content.php
+++ content.php
@@ -1,12 +1,9 @@
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text">On The Mark Tutoring</h1>
+      <h1 class="header center orange-text"><?php the_title(); ?></h1>
       <div class="row center">
-        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-      </div>
-      <div class="row center">
-        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+        <h5 class="header col s12 light"><?php the_content(); ?></h5>
       </div>
       <br><br>
 
