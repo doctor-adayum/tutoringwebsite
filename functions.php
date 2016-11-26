84459bb84b2a38298f9062dbc766b135c112bd61
doctor-adayum <ajacob7111997@gmail.com>
2016-11-26 18:11:21 +1000

    Needs updating to reflect css & js files

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+// Add scripts and stylesheets
+function onthemarktutoring_scripts() {
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/materialize.css', array(), '3.3.6' );
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/styles.css' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
