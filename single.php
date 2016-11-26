560fbd8d2da534494ce88e22ab0fbb0d78d1a965
doctor-adayum <ajacob7111997@gmail.com>
2016-11-26 18:06:48 +1000

    Update single.php

--- single.php
+++ single.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-			<?php 
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-  	
-					get_template_part( 'content-single', get_post_format() );
-  
-				endwhile; endif; 
-			?>
+	<?php 
+		if ( have_posts() ) : while ( have_posts() ) : the_post();
+			get_template_part( 'content-single', get_post_format() );
+		if ( comments_open() || get_comments_number() ) :
+	  		comments_template();
+			endif;
+
+		endwhile; endif;  
+	?>
 
 <?php get_footer(); ?>
