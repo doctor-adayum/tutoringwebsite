9a1f7a5961cfa8226620c2413d9f10201e54d188
doctor-adayum <ajacob7111997@gmail.com>
2016-11-27 19:54:09 +1000

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,12 @@
+
+<?php get_header(); ?>
+
+			<?php 
+				if ( have_posts() ) : while ( have_posts() ) : the_post();
+  	
+					get_template_part( 'content', get_post_format() );
+  
+				endwhile; endif; 
+			?>
+
+<?php get_footer(); ?>
